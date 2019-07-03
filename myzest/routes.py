b77c60a13fc65ca1338be805a0a4523113477b98
Gareth <gareth.gs13@gmail.com>
2019-07-03 16:46:37 +0200

    Recipe page
    Route endpoint, Basic layout, HTML & SCSS

--- myzest/routes.py
+++ myzest/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from myzest import app, mongo
+from bson.objectid import ObjectId
 
 
 @app.route('/')
@@ -7,3 +8,9 @@ from myzest import app, mongo
 def home():
     recipes = mongo.db.recipes.find()
     return render_template('home.html', recipes=recipes)
+
+
+@app.route('/recipe/<recipe_id>')
+def get_recipe(recipe_id):
+    recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
+    return render_template('recipe.html', recipe=recipe)
