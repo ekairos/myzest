a28238a6a9794e73ca0b0fb42e0eed5700c65e26
Gareth <gareth.gs13@gmail.com>
2019-08-19 17:01:09 +0200

    login test update
    added assertions, all tests OK

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,7 +3,11 @@ from myzest import app, mongo, bcrypt
 
 
 class TestLogin(unittest.TestCase):
-    """ Test submitted Login data and DB data match """
+    """
+    Test submitted Login data and DB data match.
+    Last 3 tests are sequenced after user DB insert
+    'test_wrong_email', 'test_wrong_password', 'test_login'
+    """
 
     test_user = {'username': "Test Name",
                  'password': bcrypt.generate_password_hash("1234"),
@@ -24,12 +28,27 @@ class TestLogin(unittest.TestCase):
 
     def test_login(self):
         mongo.db.users.insert_one(self.test_user)
-        response = self.client.post('/log_usr', data=dict({'password': "1234",
-                                                           'email': "test@mail.net"}),
+        response = self.client.post('/log_usr', data={'password': "1234",
+                                                      'email': "test@mail.net"},
                                     follow_redirects=True)
 
         self.assertEqual(response.status_code, 200)
         self.assertIn('Welcome back Test Name !', str(response.data))
+
+    def test_wrong_email(self):
+        response = self.client.post('/log_usr', data={'password': "1234",
+                                                      'email': "wrong_mail@mail.net"},
+                                    follow_redirects=True)
+
+        self.assertIn('Login unsuccessful. Please check email', str(response.data))
+
+    def test_wrong_password(self):
+        response = self.client.post('/log_usr', data={'password': "9876",
+                                                      'email': "test@mail.net"},
+                                    follow_redirects=True)
+
+        self.assertIn('Login unsuccessful. Please check email and password provided',
+                      str(response.data))
 
 
 if __name__ == "__main__":
