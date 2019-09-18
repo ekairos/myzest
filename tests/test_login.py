30a1829cc07ef1b8c35b8a2b241cd9a25da8ff71
Gareth <gareth.gs13@gmail.com>
2019-09-18 12:05:02 +0200

    update login test
    check for session object on valid login

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,9 +10,11 @@ class TestLogin(unittest.TestCase):
     'test_login', 'test_wrong_email', 'test_wrong_password'
     """
 
-    test_user = {'username': "Test Name",
-                 'password': bcrypt.generate_password_hash("1234"),
-                 'email': "test@mail.net"}
+    @classmethod
+    def setUpClass(cls):
+        cls.fake_user = {'username': "John",
+                         'password': "1234",
+                         'email': "john@mail.net"}
 
     def setUp(self):
         self.client = app.test_client()
@@ -21,36 +23,53 @@ class TestLogin(unittest.TestCase):
     def tearDownClass(cls):
         mongo.db.users.delete_many({})
 
+    @staticmethod
+    def user_to_db(user):
+        user_to_db = user.copy()
+        user_to_db['password'] = bcrypt.generate_password_hash(user_to_db['password'])
+        return mongo.db.users.insert_one(user_to_db)
+
     def test_failed_login(self):
-        response = self.client.post('/log_usr', data=self.test_user, follow_redirects=True)
+        """ Login with unregistered user data should fail """
+        response = self.client.post('/log_usr', data=self.fake_user, follow_redirects=True)
 
         self.assertEqual(response.status_code, 200)
         self.assertIn('Login unsuccessful. Please check email', str(response.data))
 
     def test_login(self):
-        mongo.db.users.insert_one(self.test_user)
-        response = self.client.post('/log_usr', data={'password': "1234",
-                                                      'email': "test@mail.net"},
-                                    follow_redirects=True)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('Welcome back Test Name !', str(response.data))
+        """
+        Login with registered user data should succeed;
+        session object should contain user's 'username' '_id' and 'favs' list;
+        A views list object should be created in session object;
+        """
+        self.user_in_db = self.user_to_db(self.fake_user)
 
         with self.client:
             self.client.get('/')
-            self.assertEqual(session['user'], {"username": self.test_user['username'],
-                                               "_id": str(self.test_user['_id'])})
+            response = self.client.post('/log_usr', data={'password': self.fake_user['password'],
+                                                          'email': self.fake_user['email']},
+                                        follow_redirects=True)
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Welcome back John !', str(response.data))
+
+            self.client.get('/')
+            self.assertEqual(session['user']['username'], self.fake_user['username'])
+            self.assertEqual(session['user']['_id'], str(self.user_in_db.inserted_id))
+            self.assertEqual(session['views'], [])
 
     def test_wrong_email(self):
-        response = self.client.post('/log_usr', data={'password': "1234",
+        """ Login should fail on wrong email submission """
+        response = self.client.post('/log_usr', data={'password': self.fake_user['password'],
                                                       'email': "wrong_mail@mail.net"},
                                     follow_redirects=True)
 
         self.assertIn('Login unsuccessful. Please check email', str(response.data))
 
     def test_wrong_password(self):
+        """ Login should fail on wrong password submission """
         response = self.client.post('/log_usr', data={'password': "9876",
-                                                      'email': "test@mail.net"},
+                                                      'email': self.fake_user['email']},
                                     follow_redirects=True)
 
         self.assertIn('Login unsuccessful. Please check email and password provided',
